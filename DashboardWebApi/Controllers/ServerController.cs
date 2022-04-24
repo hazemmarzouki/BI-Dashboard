8f801e8d5bea3a0aae3ad573d5fd82ef2102d2b9
Hazem <hazem.marzouki11@gmail.com>
2022-04-25 01:28:21 +0200

    add server controller

---
+++ DashboardWebApi/Controllers/ServerController.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using DashboardWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DashboardWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServerController : Controller
+    {
+        private readonly ApiContext _ctx;
+
+        public ServerController(ApiContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = _ctx.Servers.OrderBy(s => s.Id).ToList();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}", Name = "GetServer")]
+        public IActionResult Get(int id)
+        {
+            var response = _ctx.Servers.Find(id);
+            return Ok(response);
+        }
+
+        // "id":1,
+        //"payload":"activate"}
+        // the PUT will allows us to update an entity
+        [HttpPut("{id}")]
+        public IActionResult Message(int id, [FromBody] ServerMessage msg)
+        {
+            var server = _ctx.Servers.Find(id);
+            if (server == null)
+            {
+                return NotFound();
+            }
+            if (msg.Payload == "activate")
+            {
+                server.IsOnline = true;
+            }
+            if (msg.Payload == "deactivate")
+            {
+                server.IsOnline = false;
+            }
+            _ctx.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
---
+++ DashboardWebApi/Models/ServerMessage.cs
@@ -0,0 +1,8 @@
+namespace DashboardWebApi.Controllers
+{
+    public class ServerMessage
+    {
+        public int Id { get; set; }
+        public string Payload { get; set; }
+    }
+}
